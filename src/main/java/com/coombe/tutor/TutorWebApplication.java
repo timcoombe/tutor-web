811f95494b76954cb36dfc15b830a7cf76f586f6
Tim Coombe <tim.coombe@bskyb.com>
2015-05-23 08:04:19 +0100

    first commit

---
+++ src/main/java/com/coombe/tutor/TutorWebApplication.java
@@ -0,0 +1,12 @@
+package com.coombe.tutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorWebApplication.class, args);
+    }
+}
